b806cc04c792b20c7a422e144d04799a2ec8fd6d
NoodMan <camara.adeline@yahoo.fr>
2022-10-04 09:50:42 +0200

    First commis

--- src/main/java/Flappy.java
+++ src/main/java/Flappy.java
@@ -1,2 +1,84 @@
-package PACKAGE_NAME;public class Flappy {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Flappy extends Canvas implements KeyListener {
+
+    protected int largeurEcran = 700;
+    protected int hauteurEcran = 500;
+
+    public Flappy() throws InterruptedException {
+        JFrame fenetre = new JFrame("Flappy");
+        //On récupère le panneau de la fenetre principale
+        JPanel panneau = (JPanel) fenetre.getContentPane();
+        //On définie la hauteur / largeur de l'écran
+        panneau.setPreferredSize(new Dimension(largeurEcran, hauteurEcran));
+        setBounds(0, 0, largeurEcran, hauteurEcran);
+        //On ajoute cette classe (qui hérite de Canvas) comme composant du panneau principal
+        panneau.add(this);
+
+        fenetre.pack();
+        fenetre.setResizable(false);
+        fenetre.setLocationRelativeTo(null);
+        fenetre.setVisible(true);
+        fenetre.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        fenetre.requestFocus();
+        fenetre.addKeyListener(this);
+
+//On indique que le raffraichissement de l'ecran doit être fait manuellement.
+        createBufferStrategy(2);
+        setIgnoreRepaint(true);
+        this.setFocusable(false);
+
+
+// appel de la methode demarrer
+        demarrer();
+    }
+
+    public void demarrer() throws InterruptedException {
+
+        long indexFrame = 0;
+
+
+        while (true) {
+            indexFrame++;
+            Graphics2D dessin = (Graphics2D) getBufferStrategy().getDrawGraphics();
+
+            //-----------------------------
+            //reset dessin
+            dessin.setColor(Color.WHITE);
+            dessin.fillRect(0, 0, largeurEcran, hauteurEcran);
+
+            //LE CODE ICI <----
+
+            //-----------------------------
+            dessin.dispose();
+            getBufferStrategy().show();
+            Thread.sleep(1000 / 60);
+        }
+    }
+
+    // creation de l'objet (this)
+    public static void main(String[] args) throws InterruptedException {
+        new Flappy();
+    }
+
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        //
+        System.out.println(e.getKeyChar());
+        System.out.println(e.getKeyCode());
+    }
 }
